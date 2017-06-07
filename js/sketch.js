5ee71e89a7a68b4c5bebf37f34c7f393ad3ea1cc
Adrian Franzese <adrianfranzese@gmail.com>
2017-06-07 17:35:37 +1000

    fixed up everything

--- js/sketch.js
+++ js/sketch.js
@@ -31,58 +31,99 @@
 //  Danceability
 //  Loudness
 //  Mode (Major=1/Minor=0)
+// var authURL = "https://accounts.spotify.com/authorize"
+// var authClient = "?client_id=" + "865f5b5b201c4f16ba15284057e4f26b"
+// var authRType = "&response_type=token"
+// var authRedirect = "&redirect_uri=" + encodeURIComponent("http://0.0.0.0:8000/")
+// var authScope = "&scope=" + "user-read-currently-playing"
+// var auth = authURL + authClient + authRType + authRedirect + authScope
 
-var sToken = "BQDLnyySyp1EQ0Xgxn2jHjbsQ8EjLzYozHeP5rAZO2jO4cBejXoND5ctK1dyomj5mUC8swzzGRa7A_tDXMnv1A"
 var s = new SpotifyWebApi()
-s.setAccessToken(sToken)
 var searchList
 var mySong
 var songID = ""
-var penColH
-var penColS
-var penColB
+var ColH
+var ColS
+var ColB
 var radiusSize
 var radiusRatio = 1
-var q = 0.0
+var myInput
+
+function getURLQuery(u) {
+  var q = window.location.hash.substring(1)
+  var v = q.split('&')
+  for (var i = 0; i < v.length; i++) {
+    var pair = v[i].split("=")
+    if (pair[0] == u){return pair[1]}
+  }
+  return false
+}
+
+
+if (window.location.hash == "") {
+  window.alert("Please authorise your Spotify account")
+}
+else {
+  s.setAccessToken(getURLQuery('access_token'))
+  console.log(s.getAccessToken())
+}
+
+function searchAudioFeatures() {
+  s.getAudioFeaturesForTrack(songID, function(err, data) {
+      if (err) console.error(err)
+      else {
+        //  set mySong to the returned data object (same deal with search data - NEED TO USE FUNCTION)
+        mySong = returnData(data)
+        //  Set some vars to specific values - THE MEAT OF THE PROGRAM
+        ColH = songHue[mySong.key]                //  Set KEY to coresponding HUE (See top table)
+        ColS = Math.round(mySong.valence * 100)   //  Set VALENCE to SATURATION (rounded)
+        ColB = Math.round(mySong.energy * 100)    //  Set ENERGY to BRIGHTNESS (rounded)
+        radiusSize = Math.round(mySong.tempo)     //  Set TEMPO to RADIUS (rounded)
+      }
+  })
+}
 
 function returnData(responseData){
   return responseData
 }
 
 function searchForSong(myInput) {
+  //  set myInput to search box contents
   myInput = document.getElementById("searchBox").value
+  //  If the box is empty, dont search and set some blank vars
   if (myInput == "") {
     songID = ""
-    penColH = 0
-    penColS = 0
-    penColB = 100
+    ColH = 0
+    ColS = 0
+    ColB = 100
     radiusSize = 0
   }
+  //  Otherwise search Spotify for myInput
   else {
     s.searchTracks(myInput, function(err, data) {
+      //  If there's an error, throw an error
       if (err) console.error(err)
+      //  Otherwise continue as planned
       else {
+        //  send the returned data to a function to deal with it
+        //  (Data wont have been downloaded by the time 'return data' is run, here the function just passes
+        //  back Data once it's ready)
         searchList = returnData(data)
+        //  if the search didn't return anything, show it
         if (searchList.tracks.items == 0) {
           document.getElementById("name").innerHTML = "No Results"
           document.getElementById("artist").innerHTML = "Please try another search"
         }
+        //  Otherwise get on with it
         else {
+          //  Grap the first track's Spotify ID
           songID = searchList.tracks.items[0].id
+          //  Display the song name and artist on the page
           document.getElementById("name").innerHTML = searchList.tracks.items[0].name
           document.getElementById("artist").innerHTML = searchList.tracks.items[0].artists[0].name
-
-          s.getAudioFeaturesForTrack(songID, function(err, data) {
-              if (err) console.error(err)
-              else {
-                mySong = returnData(data)
-                penColH = songHue[mySong.key]
-                penColS = Math.round(mySong.valence * 100)
-                penColB = Math.round(mySong.energy * 100)
-                radiusSize = Math.round(mySong.tempo)
-                radiusRatio = mySong.mode
-              }
-          })
+          //  Using that track's ID, grab the Audio Features
+          searchAudioFeatures()
+          //  Clear the search box
           document.getElementById("searchBox").value = ""
           myInput = ""
         }
@@ -91,32 +132,53 @@ function searchForSong(myInput) {
   }
 }
 
+function getCurrentTrack() {
+  s.getGeneric("https://api.spotify.com/v1/me/player/currently-playing", function(err, data) {
+    if (err) console.error(err)
+    else {
+      searchList = returnData(data)
+      if (searchList.item == 0) {
+        document.getElementById("name").innerHTML = "No Results"
+        document.getElementById("artist").innerHTML = "Please try another search"
+      }
+      else {
+        songID = searchList.item.id
+        searchAudioFeatures(songID)
+        document.getElementById("name").innerHTML = searchList.item.name
+        document.getElementById("artist").innerHTML = searchList.item.artists[0].name
+      }
+    }
+  })
+}
+
 function setup() {
   var canvas = createCanvas(400,400)
   canvas.parent(mySketch)
+
 }
 
 function draw() {
   background(255)
   colorMode(HSB)
-  var c = color(penColH, penColS, penColB, 0.5)
+  var c = color(ColH, ColS, ColB, 0.5)
   // strokeWeight(1)
   // stroke(0, 0, 100)
   noStroke()
   fill(c)
   translate(width/2, height/2)
+  rotate(45)
   rectMode(RADIUS)
   angleMode(DEGREES)
   // Create ring
   // for (var i =0; i < 40; i++) {
   //   push()
   //   rotate(i / 6.35 )
-  //   triangle(0, 0, radiusSize, 0, penColB, 50)
+  //   triangle(0, 0, radiusSize, 0, ColB, 50)
   //   pop()
   // }
-  for (var i = 0; i < penColB; i++) {
-    rotate(360 / penColB)
-    rect(0,0, 1, radiusSize)
+  for (var i = 0; i < ColB; i++) {
+    rotate(540 / ColB)
+    rect(0,0, 1, radiusSize - i/2)
   }
 
   // randomSeed(Date.now())
@@ -125,7 +187,10 @@ function draw() {
   ellipse(0, 0, radiusSize*1.1, radiusSize*1.1)
   fill(c)
   ellipse(0, 0, radiusSize, radiusSize)
+  // loadImage(albArt, function(img) {
+  //   image(img, 0, 0)
+  // })
 
-  var cssCol = 'hsl(' + penColH + ',' + penColS + '%,' + penColB + '%)'
+  var cssCol = 'hsl(' + ColH + ',' + ColS + '%,' + ColB + '%)'
   select("#info").style('color', cssCol)
 }
